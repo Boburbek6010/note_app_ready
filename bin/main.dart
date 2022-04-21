c248ca93840aa52bd65386f974ce399337b9e57c
User <abdiyevnasibali>
2022-04-21 16:02:45 +0500

    second commit

--- bin/main.dart
+++ bin/main.dart
@@ -1,14 +1,31 @@
+import 'package:note_app/models/note_model.dart';
 import 'package:note_app/services/data_service.dart';
+import 'package:note_app/services/file_service.dart';
 import 'package:note_app/services/io_service.dart';
 import 'package:note_app/services/ext_service.dart';
 
 void main() async {
-
-  DataService dataService = DataService();
-  await dataService.init();
-
+  FileService fileService = FileService();
+  await fileService.init();
+  
+  String title = "";
+  String content = "";
   String exit = "";
-  writeln(await dataService.readData(key: "test"));
+  
+  write("Iltimos note ni nomini kiriting: ");
+  title = read();
+  
+  writeln("Note ni yozishingiz mumkin(Yozib tugatganingizda 'Save' so'zini yozsangiz note saqlab qo'yiladi)");
+  while(exit != "Save") {
+    exit = read();
+    if(exit == "Save") {
+      break;
+    }
+    content += (exit + "\n");
+  }
+  
+  Note note = Note(title: title, content: content);
+  await fileService.createFile(note).then((value) => print("Note muvofaqiyatli saqlandi"));
 }
 
 /*
--- lib/services/file_service.dart
+++ lib/services/file_service.dart
@@ -1,4 +1,5 @@
 import 'dart:io';
+import 'package:note_app/models/note_model.dart';
 
 class FileService {
   Directory directory = Directory(Directory.current.path + "\\assets\\files");
@@ -8,5 +9,18 @@ class FileService {
     if(!isDirectoryCreated) {
       await directory.create();
     }
+  }
+
+  Future<String> createFile(Note note) async {
+    File file = File(directory.path + "\\${note.title}.note");
+    bool isFileCreated = await file.exists();
+    if(isFileCreated) {
+      /// this below code will be edited when I set language service
+      throw Exception("This file already created please try create new file");
+    }
+    await file.create();
+    await file.writeAsString("${note.title}\n${note.content}");
+    await file.setLastModified(DateTime.parse(note.time));
+    return file.path;
   }
 }
